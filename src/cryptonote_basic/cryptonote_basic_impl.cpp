bdfa1f6dd6f270deee18e3316036d3dbc2640727
HarrisonHesslink <harrisonhesslink24@gmail.com>
2018-07-14 20:28:05 -0700

    yea

--- src/cryptonote_basic/cryptonote_basic_impl.cpp
+++ src/cryptonote_basic/cryptonote_basic_impl.cpp
@@ -142,11 +142,6 @@ namespace cryptonote {
                 if (already_generated_coins + bad_tail_emission_reward >=  MONEY_SUPPLY || base_reward < bad_tail_emission_reward)
                 {
                     base_reward = bad_tail_emission_reward;
-                     reward = base_reward;
-                    MERROR("Printed:" << print_money(fee + reward));
-                    MERROR("Already Generated:" << print_money(already_generated_coins));
-                    MERROR("Supply:" << print_money(MONEY_SUPPLY));
-                    return true;
                 }
               }
 
