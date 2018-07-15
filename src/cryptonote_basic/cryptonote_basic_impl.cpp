79459124c07bf24177087c556850c0a25e913aa1
HarrisonHesslink <harrisonhesslink24@gmail.com>
2018-07-14 20:19:08 -0700

    ahhh

--- src/cryptonote_basic/cryptonote_basic_impl.cpp
+++ src/cryptonote_basic/cryptonote_basic_impl.cpp
@@ -137,8 +137,8 @@ namespace cryptonote {
 
      uint64_t base_reward = ( MONEY_SUPPLY - already_generated_coins) >> emission_speed_factor;
       MERROR("Base Reward: " << base_reward);
-      if (already_generated_coins <=  3000000000000000000 && version == BLOCK_MAJOR_VERSION_1) {
-                uint64_t bad_tail_emission_reward = ((uint64_t)3000000000000000000);
+      if (already_generated_coins <=  300000000 && version == BLOCK_MAJOR_VERSION_1) {
+                uint64_t bad_tail_emission_reward = ((uint64_t)300000000);
                 if (already_generated_coins + bad_tail_emission_reward >=  MONEY_SUPPLY || base_reward < bad_tail_emission_reward)
                 {
                     base_reward = bad_tail_emission_reward;
