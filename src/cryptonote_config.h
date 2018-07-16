5bc4920b8bd90d6ee3ad6e6b96af0c416aa749b8
HarrisonHesslink <harrisonhesslink24@gmail.com>
2018-07-16 03:29:19 -0700

    yea

--- src/cryptonote_config.h
+++ src/cryptonote_config.h
@@ -71,12 +71,12 @@
 #define CRYPTONOTE_COINBASE_BLOB_RESERVED_SIZE          600
 #define CRYPTONOTE_DISPLAY_DECIMAL_POINT                11
 // COIN - number of smallest units in one coin
-#define COIN                                            ((uint64_t)100000000000) // pow(10, 2)
-#define LEGACY_MINIMUM_FEE								              ((uint64_t)1000000)
+#define COIN                                            ((uint64_t)10000000000) // pow(10, 2)
+#define LEGACY_MINIMUM_FEE								              ((uint64_t)100000)
 #define FEE_PER_KB_OLD                                  ((uint64_t)10000000000) // pow(10, 10)
 #define FEE_PER_KB                                      ((uint64_t)2000000000) // 2 * pow(10, 9)
 #define DYNAMIC_FEE_PER_KB_BASE_FEE                     ((uint64_t)2000000000) // 2 * pow(10,9)
-#define DYNAMIC_FEE_PER_KB_BASE_BLOCK_REWARD            ((uint64_t)10000000000000) // 10 * pow(10,12)
+#define DYNAMIC_FEE_PER_KB_BASE_BLOCK_REWARD            ((uint64_t)1000000000000) // 10 * pow(10,12)
 #define DYNAMIC_FEE_PER_KB_BASE_FEE_V5                  ((uint64_t)2000000000 * (uint64_t)CRYPTONOTE_BLOCK_GRANTED_FULL_REWARD_ZONE_V2 / CRYPTONOTE_BLOCK_GRANTED_FULL_REWARD_ZONE_V5)
 
 #define ORPHANED_BLOCKS_MAX_COUNT                       100
