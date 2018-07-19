dc63e99f82c4bce44a99c16036cc5787ce03d955
HarrisonHesslink <harrisonhesslink24@gmail.com>
2018-07-18 23:06:22 -0700

    yea

--- src/cryptonote_core/blockchain.cpp
+++ src/cryptonote_core/blockchain.cpp
@@ -1104,11 +1104,13 @@ bool Blockchain::validate_miner_transaction(const block& b, size_t cumulative_bl
     return false;
   }
 }
+  if(m_db->height() >=2){
       CHECK_AND_ASSERT_MES(money_in_use - fee <= base_reward, false, "base reward calculation bug");
     if(base_reward + fee != money_in_use){
       partial_block_reward = true;
     base_reward = money_in_use - fee;
 	}
+}
   return true;
 }
 //------------------------------------------------------------------
