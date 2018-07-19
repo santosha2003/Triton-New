2a670bdf1ffb707887508bdda9d98e3926a861b9
HarrisonHesslink <harrisonhesslink24@gmail.com>
2018-07-19 12:13:02 -0700

    ytest

--- src/cryptonote_config.h
+++ src/cryptonote_config.h
@@ -58,7 +58,7 @@
 #define BLOCKCHAIN_TIMESTAMP_CHECK_WINDOW               60
 
 // MONEY_SUPPLY - total number coins to be generated
-#define MONEY_SUPPLY                                   840000000000000000
+#define MONEY_SUPPLY                                   84000000000000000000
 #define PREMINE                                        300000000000000000
 
 #define EMISSION_SPEED_FACTOR_PER_MINUTE                (20)
@@ -69,9 +69,9 @@
 #define CRYPTONOTE_BLOCK_GRANTED_FULL_REWARD_ZONE_V2    1000000
 #define CRYPTONOTE_BLOCK_GRANTED_FULL_REWARD_ZONE_V5    1000000
 #define CRYPTONOTE_COINBASE_BLOB_RESERVED_SIZE          600
-#define CRYPTONOTE_DISPLAY_DECIMAL_POINT                10
+#define CRYPTONOTE_DISPLAY_DECIMAL_POINT                12
 // COIN - number of smallest units in one coin
-#define COIN                                            ((uint64_t)100000000000) // pow(10, 2)
+#define COIN                                            ((uint64_t)1000000000000) // pow(10, 2)
 #define LEGACY_MINIMUM_FEE								              ((uint64_t)100000)
 #define FEE_PER_KB_OLD                                  ((uint64_t)10000000000) // pow(10, 10)
 #define FEE_PER_KB                                      ((uint64_t)2000000000) // 2 * pow(10, 9)
