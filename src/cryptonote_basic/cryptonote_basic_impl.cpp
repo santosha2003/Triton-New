35c37a6ab0a27062cbf7998f4b4b44ffd025cc0d
HarrisonHesslink <harrisonhesslink24@gmail.com>
2018-07-19 22:42:24 -0700

    sed

--- src/cryptonote_basic/cryptonote_basic_impl.cpp
+++ src/cryptonote_basic/cryptonote_basic_impl.cpp
@@ -122,14 +122,13 @@ namespace cryptonote {
     const int target = DIFFICULTY_TARGET_V2;
     const int target_minutes = target / 60;
     const int emission_speed_factor = EMISSION_SPEED_FACTOR_PER_MINUTE;
-    unsigned long long supply = 84000000000000000000ull;
     if(height == 0){
       reward = 0000000000000;
       return true;
     }
     if (already_generated_coins == 0)
    {
-     reward = 3000000000000000000ull;
+     reward = 300000000000000;
      return true;
    }
     uint64_t full_reward_zone = get_min_block_size(version);
@@ -144,13 +143,13 @@ namespace cryptonote {
        return false;
      }
 
-     uint64_t base_reward = ( supply - already_generated_coins) >> emission_speed_factor;
+     uint64_t base_reward = ( MONEY_SUPPLY - already_generated_coins) >> emission_speed_factor;
 
      reward = get_penalized_amount(base_reward, median_size, current_block_size);
       reward +=  version < BLOCK_MAJOR_VERSION_7 ? get_penalized_amount(fee, median_size, current_block_size) : fee;
      MERROR("Printed:" << print_money(fee + reward));
      MERROR("Already Generated:" << print_money(already_generated_coins));
-     MERROR("Supply:" << print_money(supply));
+     MERROR("Supply:" << print_money(MONEY_SUPPLY));
      MERROR("Base Reward:" << print_money(base_reward));
      MERROR("Reward:" << print_money(reward));
 
