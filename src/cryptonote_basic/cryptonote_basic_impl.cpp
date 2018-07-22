0c2001b768b613085f72f205997a37400f120dc0
HarrisonHesslink <harrisonhesslink24@gmail.com>
2018-07-21 21:37:32 -0700

    ya

--- src/cryptonote_basic/cryptonote_basic_impl.cpp
+++ src/cryptonote_basic/cryptonote_basic_impl.cpp
@@ -126,7 +126,7 @@ namespace cryptonote {
     if(height == 0){
       reward = 0000000000000;
       MERROR("Already Generated:" << print_money(already_generated_coins));
-      MERROR("Supply:" << print_money((MONEY_SUPPLY * yeet)));
+      MERROR("Supply:" << print_money((MONEY_SUPPLY)));
       MERROR("Reward:" << print_money(reward));
       return true;
     }
