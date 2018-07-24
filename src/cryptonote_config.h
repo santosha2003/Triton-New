71e8c055746b94ca08e9db733c9fa221eb93185b
HarrisonHesslink <harrisonhesslink24@gmail.com>
2018-07-24 06:53:03 -0700

    fix

--- src/cryptonote_config.h
+++ src/cryptonote_config.h
@@ -93,7 +93,7 @@
 #define DIFFICULTY_CUT                                  60  // timestamps to cut after sorting
 #define DIFFICULTY_CUT_V2                               6  // timestamps to cut after sorting
 
-#define DIFFICULTY_BLOCKS_COUNT                         DIFFICULTY_WINDOW + DIFFICULTY_CUT
+#define DIFFICULTY_BLOCKS_COUNT                         DIFFICULTY_WINDOW + DIFFICULTY_LAG
 #define DIFFICULTY_BLOCKS_COUNT_V2                      DIFFICULTY_WINDOW_V2 + DIFFICULTY_CUT_V2
 
 
