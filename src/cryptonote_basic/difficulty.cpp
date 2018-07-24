bedb557d5bf2f95d3ec2fead2a6480ac1e3232df
HarrisonHesslink <harrisonhesslink24@gmail.com>
2018-07-24 06:05:16 -0700

    fix

--- src/cryptonote_basic/difficulty.cpp
+++ src/cryptonote_basic/difficulty.cpp
@@ -173,7 +173,6 @@ namespace cryptonote {
          return nextDiffZ;
 
      }
-  }
     difficulty_type next_difficulty_v2(std::vector<std::uint64_t> timestamps, std::vector<difficulty_type> cumulative_difficulties, size_t target_seconds, size_t height) {
       if(height <= 24922){return 10000000;}
       int64_t T = target_seconds;
