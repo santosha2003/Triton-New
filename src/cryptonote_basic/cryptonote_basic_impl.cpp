37f2c65a065f862209778554b7e8423685dc1de5
HarrisonHesslink <harrisonhesslink24@gmail.com>
2018-07-26 22:22:34 -0700

    oops

--- src/cryptonote_basic/cryptonote_basic_impl.cpp
+++ src/cryptonote_basic/cryptonote_basic_impl.cpp
@@ -154,14 +154,15 @@ namespace cryptonote {
          return false;
        }
        uint64_t base_reward = (MONEY_SUPPLYv7 - already_generated_coins) >> emission_speed_factor;
-       reward = get_penalized_amount((base_reward * 100), median_size, current_block_size);
+       reward = get_penalized_amount((base_reward), median_size, current_block_size);
         reward +=  version < BLOCK_MAJOR_VERSION_7 ? get_penalized_amount(fee, median_size, current_block_size) : fee;
+
 
      }else if(version < 7){
        uint64_t base_reward = (MONEY_SUPPLY - already_generated_coins) >> emission_speed_factor;
        MERROR(base_reward);
 
-       reward = get_penalized_amount((base_reward), median_size, current_block_size);
+       reward = get_penalized_amount((base_reward * 100), median_size, current_block_size);
        MERROR(reward);
         reward +=  version < BLOCK_MAJOR_VERSION_7 ? get_penalized_amount(fee, median_size, current_block_size) : fee;
 
