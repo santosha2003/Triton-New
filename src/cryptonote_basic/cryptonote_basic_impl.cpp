3ad5780f05022a92b955447d8d037583219b2d18
HarrisonHesslink <harrisonhesslink24@gmail.com>
2018-07-28 00:41:34 -0700

    yeet

--- src/cryptonote_basic/cryptonote_basic_impl.cpp
+++ src/cryptonote_basic/cryptonote_basic_impl.cpp
@@ -159,9 +159,7 @@ namespace cryptonote {
 
 
      }else if(version < 7){
-       uint64_t base_reward = (MONEY_SUPPLY - already_generated_coins) >> emission_speed_factor;
-
-       reward = get_penalized_amount((base_reward), median_size, current_block_size);
+       reward = 77;
         reward +=  version < BLOCK_MAJOR_VERSION_7 ? get_penalized_amount(fee, median_size, current_block_size) : fee;
 
     }
