589e1492100f4c8e5ce75f0a02fb2c3275e831fb
Harrison Hesslink <harrisonhesslink24@gmail.com>
2018-09-05 10:41:23 +0300

    Forgot to change this

--- src/cryptonote_basic/difficulty.cpp
+++ src/cryptonote_basic/difficulty.cpp
@@ -211,7 +211,7 @@ namespace cryptonote {
       if (nextDiffZ <= 1) {
         nextDiffZ = 1;
       }
-      if(height == 27777){
+      if(height == 28082){
 
         printf("NextDiff:%lu",nextDiffZ);
 
