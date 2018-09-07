37ff45ae1b123d6eb4c9ccddbb7277822258139e
Harrison Hesslink <harrisonhesslink24@gmail.com>
2018-09-07 23:37:00 +0300

    fix diff_b_C

--- src/cryptonote_core/blockchain.cpp
+++ src/cryptonote_core/blockchain.cpp
@@ -791,7 +791,7 @@ difficulty_type Blockchain::get_difficulty_for_next_block()
  }else if(version >= 5 && version < 7){
    difficulty_blocks_count = DIFFICULTY_BLOCKS_COUNT;
  }else if(version >= 7){
-   difficulty_blocks_count = DIFFICULTY_BLOCKS_COUNT_V7;
+   difficulty_blocks_count = DIFFICULTY_BLOCKS_COUNT_V3;
  }
   // ND: Speedup
   // 1. Keep a list of the last 735 (or less) blocks that is used to compute difficulty,
