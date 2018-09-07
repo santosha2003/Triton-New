a95a460fbe9ea68906a063e54151ff20e7ecb8c7
Harrison Hesslink <harrisonhesslink24@gmail.com>
2018-09-08 01:37:25 +0300

    fix median_size

--- src/cryptonote_basic/cryptonote_basic_impl.cpp
+++ src/cryptonote_basic/cryptonote_basic_impl.cpp
@@ -163,7 +163,7 @@ namespace cryptonote {
 
 
      size_t blockGrantedFullRewardZone = CRYPTONOTE_BLOCK_GRANTED_FULL_REWARD_ZONE_V1;
-     medianSize = std::max(medianSize, blockGrantedFullRewardZone);
+     median_size = std::max(median_size, blockGrantedFullRewardZone);
 
      if (current_block_size > 2 * medianSize) {
        MERROR("Block cumulative size is too big: " << current_block_size << ", expected less than " << 2 * median_size);
