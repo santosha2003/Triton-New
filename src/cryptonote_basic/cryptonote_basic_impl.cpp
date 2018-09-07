82b14d54d53f3d5100d6fd487979673353684bb1
Harrison Hesslink <harrisonhesslink24@gmail.com>
2018-09-08 01:22:29 +0300

    change

--- src/cryptonote_basic/cryptonote_basic_impl.cpp
+++ src/cryptonote_basic/cryptonote_basic_impl.cpp
@@ -159,7 +159,7 @@ namespace cryptonote {
 
 
      }else {
-       uint64_t base_reward = (MONEY_SUPPLYv7 - already_generated_coins) >> emission_speed_factor;
+       uint64_t base_reward = (MONEY_SUPPLY - already_generated_coins) >> emission_speed_factor;
        reward = get_penalized_amount((base_reward), median_size, current_block_size);
         reward +=  version < BLOCK_MAJOR_VERSION_7 ? get_penalized_amount(fee, median_size, current_block_size) : fee;
      }
