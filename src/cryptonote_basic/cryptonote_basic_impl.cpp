b9b3226a61ddf4a7fb2edbad19b0ab83dfea3c9b
Harrison Hesslink <harrisonhesslink24@gmail.com>
2018-09-08 16:19:22 +0300

    test

--- src/cryptonote_basic/cryptonote_basic_impl.cpp
+++ src/cryptonote_basic/cryptonote_basic_impl.cpp
@@ -158,7 +158,7 @@ namespace cryptonote {
          return true;
 
    }else{
-     uint64_t baseReward = (84000000 - (already_generated_coins)) >> emission_speed_factor;
+     uint64_t baseReward = (84000000 - already_generated_coins / 10) >> emission_speed_factor;
      MERROR("BR: " << baseReward << " AGC: " << already_generated_coins);
 
 
