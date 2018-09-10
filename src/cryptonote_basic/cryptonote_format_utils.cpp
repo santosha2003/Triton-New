f2a6f633c75e9b1136f585068474d133e9fd9b9d
Harrison Hesslink <harrisonhesslink24@gmail.com>
2018-09-10 20:54:10 +0300

    Fix

--- src/cryptonote_basic/cryptonote_format_utils.cpp
+++ src/cryptonote_basic/cryptonote_format_utils.cpp
@@ -1134,8 +1134,10 @@ namespace cryptonote
   {
 	  switch (bl.major_version)
 	  {
-      case BLOCK_MAJOR_VERSION_2:
+      case BLOCK_MAJOR_VERSION_1:
         return check_proof_of_work_v1(bl, current_diffic, proof_of_work);
+      case BLOCK_MAJOR_VERSION_2:
+        return check_proof_of_work_v2(bl, current_diffic, proof_of_work);
       case BLOCK_MAJOR_VERSION_3:
         return check_proof_of_work_v2(bl, current_diffic, proof_of_work);
  	    case BLOCK_MAJOR_VERSION_4:
@@ -1144,7 +1146,6 @@ namespace cryptonote
         return check_proof_of_work_v2(bl,current_diffic,proof_of_work);
       case BLOCK_MAJOR_VERSION_7:
         return check_proof_of_work_v2(bl,current_diffic,proof_of_work);
-
 	  }
 
 	  CHECK_AND_ASSERT_MES(false, false, "unknown block major version: " << bl.major_version << "." << bl.minor_version);
