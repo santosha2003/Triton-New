3349dbbae43275c11963cf7d5ebf9bee93e645c3
Harrison Hesslink <harrisonhesslink24@gmail.com>
2018-09-14 21:44:46 +0200

    diff block count

--- src/cryptonote_core/blockchain.cpp
+++ src/cryptonote_core/blockchain.cpp
@@ -787,7 +787,12 @@ difficulty_type Blockchain::get_difficulty_for_next_block()
   std::vector<difficulty_type> difficulties;
   auto height = m_db->height() - 1;
   uint8_t version = get_current_hard_fork_version();
-  size_t difficulty_blocks_count = version < BLOCK_MAJOR_VERSION_2 ? DIFFICULTY_BLOCKS_COUNT : DIFFICULTY_BLOCKS_COUNT_V2;
+  size_t difficulty_blocks_count;
+  if(version >= 2 && version < 7){
+    difficulty_blocks_count = DIFFICULTY_BLOCKS_COUNT;
+  }else if(version >= 7){
+    difficulty_blocks_count = DIFFICULTY_BLOCKS_COUNT_V3;
+  }
   // ND: Speedup
   // 1. Keep a list of the last 735 (or less) blocks that is used to compute difficulty,
   //    then when the next block difficulty is queried, push the latest height data and
