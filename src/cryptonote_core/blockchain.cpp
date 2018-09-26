4434c42ec3cb76944d881adc2fb75f3ed6b04a49
Harrison Hesslink <harrisonhesslink24@gmail.com>
2018-09-25 20:10:36 -0700

    add easy diff transition

--- src/cryptonote_core/blockchain.cpp
+++ src/cryptonote_core/blockchain.cpp
@@ -1000,6 +1000,9 @@ difficulty_type Blockchain::get_difficulty_for_next_block()
 }
 
   }else if(version >= 7){
+    if(height >= 90810 && <= 90810 + DIFFICULTY_WINDOW_V3){
+      diff = 10000;
+    }
      diff = next_difficulty_v3(std::move(timestamps), std::move(difficulties), target,height - 1);
   }
   m_difficulty_for_next_block_top_hash = top_hash;
