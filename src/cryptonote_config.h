699a721c3f442eba782a08f4f5bf9ddb9eeba05c
Harrison Hesslink <harrisonhesslink24@gmail.com>
2018-10-23 20:01:23 -0700

    more changes

--- src/cryptonote_config.h
+++ src/cryptonote_config.h
@@ -80,7 +80,7 @@
 #define DIFFICULTY_TARGET_V1                            180  // seconds - before first fork
 #define DIFFICULTY_WINDOW                               720 // blocks
 #define DIFFICULTY_WINDOW_V1                           60
-#define DIFFICULTY_BLOCKS_COUNT_V1                     DIFFICULTY_WINDOW_V3 + 1
+#define DIFFICULTY_BLOCKS_COUNT_V1                     DIFFICULTY_WINDOW_V1 + 1
 #define DIFFICULTY_LAG                                  15  // !!!
 #define DIFFICULTY_CUT                                  60  // timestamps to cut after sorting
 #define DIFFICULTY_BLOCKS_COUNT                         DIFFICULTY_WINDOW + DIFFICULTY_LAG
